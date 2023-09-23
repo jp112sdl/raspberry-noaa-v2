73804924e7a0539db65c1018eb21646646e1cb19
jp112sdl <jp112sdl@icloud.com>
2023-09-23 19:09:48 +0200

    sometimes not all images are deleted

--- scripts/prune_scripts/prune_older_than.sh
+++ scripts/prune_scripts/prune_older_than.sh
@@ -20,3 +20,5 @@ for img_path in $(sqlite3 ${DB_FILE} "select file_path from decoded_passes where
   sqlite3 "${DB_FILE}" "delete from decoded_passes where file_path = \"$img_path\";"
   log "  Database entry pruned" "INFO"
 done
+find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -mtime +$PRUNE_OLDER_THAN -delete;
+find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -mtime +$PRUNE_OLDER_THAN -delete;
