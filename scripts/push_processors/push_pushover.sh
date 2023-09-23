dfd31aea1704ce3e7731b5cb615b2db8e43e7312
jp112sdl <jp112sdl@icloud.com>
2023-09-23 19:09:28 +0200

    add pushover

---
+++ scripts/push_processors/push_pushover.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# import common lib and settings
+. "$HOME/.noaa-v2.conf"
+. "$NOAA_HOME/scripts/common.sh"
+APITOKEN=${PUSHOVER_APITOKEN}
+USER=${PUSHOVER_USER}
+PRIO=${PUSHOVER_PRIO}
+TEXT=$1
+SAT=$2
+FILELIST=$3
+push_attachment_file=""
+
+for singlefile in $FILELIST; do
+ if [[ $singlefile == *"-221_corrected"* ]]; then
+   push_attachment_file=$singlefile
+   break
+ fi
+ if [[ $singlefile == *"MSA"* ]]; then
+   push_attachment_file=$singlefile
+   break
+ fi
+ if [[ $singlefile == *"MCIR"* ]]; then
+   push_attachment_file=$singlefile
+ fi
+done
+
+push_attachment=""
+if [ -f "$push_attachment_file" ] && [ ! -z push_attachment_file ]; then
+  push_attachment="-F attachment=@${push_attachment_file}"
+else
+  log "Pushover - no image to attach!" "WARN"
+fi
+
+log "Pushing to Pushover" "INFO"
+log "Pushover filelist: ${FILELIST}" "INFO"
+log "Pushover is using attachment: ${push_attachment_file}" "INFO"
+
+
+# timeout 20 curl -s -k -d token=${APITOKEN} -d user=${USER} -d message="${TEXT}" -d title="${SAT}" -d priority=${PRIO} -d html=1 https://api.pushover.net/1/messages.json
+timeout 20 curl -s -k --form-string "token=${APITOKEN}" --form-string "user=${USER}" --form-string "message=${TEXT}" --form-string "title=${SAT}" --form-string "priority=${PRIO}" --form-string "html=1" ${push_attachment} https://api.pushover.net/1/messages.json
+
