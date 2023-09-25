ba8d2c0d9a79608e0726ead8bde158031391a992
jp112sdl <jp112sdl@icloud.com>
2023-09-25 22:25:07 +0200

    pushover: if filesize is too large, we have to rescale

--- scripts/push_processors/push_pushover.sh
+++ scripts/push_processors/push_pushover.sh
@@ -3,6 +3,7 @@
 # import common lib and settings
 . "$HOME/.noaa-v2.conf"
 . "$NOAA_HOME/scripts/common.sh"
+FSIZE_LIMIT=2621440
 APITOKEN=${PUSHOVER_APITOKEN}
 USER=${PUSHOVER_USER}
 PRIO=${PUSHOVER_PRIO}
@@ -27,12 +28,20 @@ done
 
 push_attachment=""
 if [ -f "$push_attachment_file" ] && [ ! -z push_attachment_file ]; then
+  fsize=`stat -c%s ${push_attachment_file}`
+  if [ "$fsize" -gt "$FSIZE_LIMIT" ]; then
+    echo "File too large. Need to resize"
+    ffmpeg -y -i ${push_attachment_file} -vf "scale=iw*0.8:-1" /tmp/rn2.jpg
+    push_attachment_file=/tmp/rn2.jpg 
+  fi
   push_attachment="-F attachment=@${push_attachment_file}"
 else
   log "Pushover - no image to attach!" "WARN"
 fi
 
 log "Pushing to Pushover" "INFO"
+log "Pushover text: ${TEXT}" "INFO"
+log "Pushover sat: ${SAT}" "INFO"
 log "Pushover filelist: ${FILELIST}" "INFO"
 log "Pushover is using attachment: ${push_attachment_file}" "INFO"
 
